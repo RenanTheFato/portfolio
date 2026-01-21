314ba1f05938868f3407dcdbe347ac18b097a430
RenanTheFato <renan.santana007@hotmail.com>
2026-01-21 18:10:23 -0300

    add the terminal and put the callback from topbar animation as a conditional

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
+'use client'
+
+import { useState } from "react";
+import { Terminal } from "@/components/terminal";
 import { Topbar } from "@/components/topbar";
 
 export default function Home() {
+  const [topbarComplete, setTopbarComplete] = useState(false)
+
   return (
     <div className="w-screen h-screen flex flex-col">
-      <Topbar />
+      <Topbar onAnimationComplete={() => setTopbarComplete(true)} />
+      <main className="flex-1 overflow-hidden">
+        <Terminal startAnimation={topbarComplete} />
+      </main>
     </div>
   )
 }
