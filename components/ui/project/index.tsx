64a416774fa8df5f17fca9c8e7d589868073f6ef
RenanTheFato <renan.santana007@hotmail.com>
2026-03-12 15:54:49 -0300

    create project component, displays an screenshot, title and badges content

---
+++ components/ui/project/index.tsx
@@ -0,0 +1,47 @@
+import Image from "next/image";
+import { ReactNode } from "react";
+
+interface ProjectProps {
+  children: ReactNode,
+}
+
+interface ProjectImageProps {
+  src: string,
+  alt?: string
+}
+
+function Project({ children }: ProjectProps) {
+  return (
+    <div className="flex flex-col items-start justify-between border border-white/20 rounded-md overflow-hidden">
+      {children}
+    </div>
+  )
+}
+
+function Content({ children }: ProjectProps) {
+  return <div className="flex flex-col gap-2 p-3 w-full">{children}</div>
+}
+
+function ProjectImage({ src, alt }: ProjectImageProps) {
+  return (
+    <div className="relative w-full h-36">
+      <Image src={src} alt={alt || "image"} fill className="object-cover group-hover:scale-105 transition-transform duration-300" />
+    </div>
+  )
+}
+
+function Title({ children }: ProjectProps) {
+  return <span className="text-white font-brains text-base">{children}</span>
+}
+
+function Badges({ children }: ProjectProps) {
+  return <div className="flex flex-row flex-wrap gap-1">{children}</div>
+}
+
+
+Project.Image = ProjectImage
+Project.Title = Title
+Project.Badges = Badges
+Project.Content = Content
+
+export { Project }
