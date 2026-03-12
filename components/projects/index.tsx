c1436f3b183d17e3b0623970a4e1b0b6efaea6d0
RenanTheFato <renan.santana007@hotmail.com>
2026-03-12 15:56:20 -0300

    add projects section

---
+++ components/projects/index.tsx
@@ -0,0 +1,9 @@
+import { Window } from "../ui/window";
+
+export function Projects(){
+  return (
+    <div className="relative w-full h-full flex flex-col p-8 bg-black border-t border-t-white/30 overflow-hidden">
+      <Window />
+    </div>
+  )
+}
