c5004bcd4c450bac7bd902721bda6089d10fd4c4
RenanTheFato <renan.santana007@hotmail.com>
2026-03-14 17:57:02 -0300

    add padding on x axis

--- components/projects/index.tsx
+++ components/projects/index.tsx
@@ -7,7 +7,7 @@ import { ProjectProvider } from "@/contexts/project-context"
 export function Projects() {
   return (
     <ProjectProvider>
-      <div className="relative w-full h-full flex flex-row p-8 space-x-8 bg-black border-t border-t-white/30 overflow-hidden">
+      <div className="relative w-full h-full flex flex-row p-8 px-24 space-x-8 bg-black border-t border-t-white/30 overflow-hidden">
         <Window />
         <Visualizer />
       </div>
