3fec4e53653b1669e7023335327093017fb1560d
RenanTheFato <renan.santana007@hotmail.com>
2026-03-14 21:08:21 -0300

    add new data type to increase certificate section

---
+++ types/certification-data.ts
@@ -0,0 +1,10 @@
+export interface CertificationData {
+  name: string,
+  description: string,
+  issuing_organization: string,
+  issue_date: string,
+  organization_image: string,
+  credential_id: string
+  url: string,
+  source: string,
+}
