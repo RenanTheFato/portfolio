1d76ed92d1341fbcebedb4514546b78dcd884ccb
RenanTheFato <renan.santana007@hotmail.com>
2026-03-14 21:08:34 -0300

    add certification data

---
+++ data/certifications.ts
@@ -0,0 +1,14 @@
+import { CertificationData } from "@/types/certification-data";
+
+export const certifications: CertificationData[] = [
+  {
+    name: "Harvard CS50x",
+    description: "Harvard CS50",
+    issuing_organization: "Harvard",
+    issue_date: "March 2026",
+    organization_image: "harvard-logo.png",
+    credential_id: "5c1c7a40-573c-4c91-875b-c815292c39a8",
+    url: "https://certificates.cs50.io/5c1c7a40-573c-4c91-875b-c815292c39a8.pdf?size=letter",
+    source: "CS50x.png"
+  },
+]
