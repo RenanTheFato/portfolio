bcb0f241a36e0e1c1dac72225810852a24dce3a8
RenanTheFato <renan.santana007@hotmail.com>
2026-03-17 15:33:09 -0300

    add cpu component data types

---
+++ types/contatct-chip.ts
@@ -0,0 +1,10 @@
+export interface ContactChipProps {
+  label: string
+  value: string
+  href: string
+  icon: React.ElementType
+  color: string
+  accentBg: string
+  accentBorder: string
+  copyable?: boolean
+}
