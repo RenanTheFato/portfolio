f86290b59a1de482f8236516fcaff5011711904f
RenanTheFato <renan.santana007@hotmail.com>
2026-03-17 14:32:54 -0300

    add new certificates

--- data/certifications.ts
+++ data/certifications.ts
@@ -11,4 +11,34 @@ export const certifications: CertificationData[] = [
     url: "https://certificates.cs50.io/5c1c7a40-573c-4c91-875b-c815292c39a8.pdf?size=letter",
     source: "CS50x.png"
   },
+  {
+    name: "Venturus Hackathon 2nd place",
+    description: "Venturus",
+    issuing_organization: "Venturus",
+    issue_date: "December 2025",
+    organization_image: "venturus-logo.png",
+    credential_id: "",
+    url: "https://github.com/RenanTheFato/Hisui",
+    source: "venturus-hackathon.png"
+  },
+  {
+    name: "NLW Agents Advanced Level",
+    description: "NLW Agents",
+    issuing_organization: "Rocketseat",
+    issue_date: "July 2025",
+    organization_image: "rocketseat-logo.jpeg",
+    credential_id: "2bdd6a56-d06a-453e-a65b-12f1f64f0a69",
+    url: "https://app.rocketseat.com.br/certificates/2bdd6a56-d06a-453e-a65b-12f1f64f0a69",
+    source: "nlw-agents.jpeg"
+  },
+  {
+    name: "NLW Operator Advanced Level",
+    description: "NLW Operator",
+    issuing_organization: "Rocketseat",
+    issue_date: "March 2026",
+    organization_image: "rocketseat-logo.jpeg",
+    credential_id: "a0b2f28b-66a0-43d4-84af-36b0006ff9b0",
+    url: "https://app.rocketseat.com.br/certificates/a0b2f28b-66a0-43d4-84af-36b0006ff9b0",
+    source: "nlw-operator.png"
+  },
 ]
