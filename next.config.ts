e8abb7fc6ec7075d17c214059290f657f4425fba
RenanTheFato <renan.santana007@hotmail.com>
2026-03-19 17:29:41 -0300

    add next new configs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import { NextConfig } from 'next'
+import createNextIntlPlugin from 'next-intl/plugin'
+
+const withNextIntl = createNextIntlPlugin('./translator/request.ts')
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  images: {
+    qualities: [75, 95, 100]
+  },
+}
 
-export default nextConfig;
+export default withNextIntl(nextConfig)
